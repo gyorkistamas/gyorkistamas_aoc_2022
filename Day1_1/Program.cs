146ace690a5b9aa17022bef8dbc48df5074bb84b
Tamás Györkis <tamasgyorkis@gmail.com>
2022-12-02 21:22:52 +0100

    Add project files.

---
+++ Day1_1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Első rész
+            List<int> list = new List<int>();
+
+            string[] data = File.ReadAllLines("input.txt");
+
+            int i = 0;
+            while(i < data.Length)
+            {
+                int sum = 0;
+
+                while (i < data.Length && data[i] != "")
+                {
+                    sum += Convert.ToInt32(data[i]);
+                    i++;
+                }
+                i++;
+                list.Add(sum);
+            }
+
+
+            Console.WriteLine(list.Max());
+            Console.ReadLine();
+
+            // Második rész
+
+            list.Sort();
+            list.Reverse();
+            int sumOfThree = list.Take(3).Sum();
+            Console.WriteLine(sumOfThree);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day_2/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Day_2
+{
+    internal class Program
+    {
+        
+
+        static Dictionary<string, int> Shape = new Dictionary<string, int>()
+        {
+            {"X", 1 },
+            {"Y", 2},
+            {"Z", 3},
+        };
+
+
+        static int GetResult(string shapes) => shapes switch
+        {
+            "AX" or "BY" or "CZ" => 3,
+            "AY" or "BZ" or "CX" => 6,
+            _ => 0
+
+        };
+
+        static string GetStepToDraw(string shape) => shape switch
+        {
+            "A" => "X",
+            "B" => "Y",
+            "C" => "Z",
+        };
+
+        static string GetStepWin(string shape) => shape switch
+        {
+            "A" => "Y",
+            "B" => "Z",
+            "C" => "X"
+        };
+
+        static string GetStepLose(string shape) => shape switch
+        {
+            "A" => "Z",
+            "B" => "X",
+            "C" => "Y"
+        };
+
+        static void Main(string[] args)
+        {
+
+            string[] data = File.ReadAllLines("input.txt");
+
+            int sum = 0;
+
+            foreach (var item in data)
+            {
+                string move = Convert.ToString(item[0])  + Convert.ToString(item[2]);
+
+                sum += Shape[Convert.ToString(move[1])];
+
+                sum += GetResult(move);
+            }
+
+            Console.ReadLine();
+
+            int sum2 = 0;
+
+            foreach (var item in data)
+            {
+                string move = Convert.ToString(item[0]) + Convert.ToString(item[2]);
+
+                string shapeToShow = Convert.ToString(move[1]) switch
+                {
+                    "X" => GetStepLose(Convert.ToString(move[0])),
+                    "Y" => GetStepToDraw(Convert.ToString(move[0])),
+                    "Z" => GetStepWin(Convert.ToString(move[0])),
+                    _ => ""
+                };
+
+                sum2 += Shape[shapeToShow];
+
+                string actualRound = Convert.ToString(move[0]) + shapeToShow;
+
+                sum2 += GetResult(actualRound);
+            }
+
+
+            Console.WriteLine(sum2);
+            Console.ReadLine();
+        }
+    }
+}
